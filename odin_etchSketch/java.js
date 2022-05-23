c769d2618c41e9aaac3f4dab71a2f72273b4258a
Jan <jrejasii@gmail.com>
2022-05-23 16:22:29 -0500

    can draw inside the box now

--- odin_etchSketch/java.js
+++ odin_etchSketch/java.js
@@ -1,3 +1,13 @@
+let color = "black"; //Im dumb by making this a global variable it can be changed WTF   
+const buttonBlack = document.querySelector('.black');
+const buttonInvert = document.querySelector('.invert');
+const buttonErase = document.querySelector('.erase');
+const buttonOpacity = document.querySelector('.opacity'); //needs to be a slider
+let buttonRandom = document.querySelector('.random');
+const buttonReset = document.querySelector('.reset');
+
+
+
 //reveiw the whole script!!
 function createGrid(size){
     let board = document.querySelector(".board");
@@ -10,14 +20,60 @@ function createGrid(size){
     let val = size * size;
     for (let i = 0; i < val; i++){
         let box = document.createElement('div');
-        box.style.backgroundColor = 'red';
+        box.classList.add('box');
+        box.addEventListener('mouseover', colorSquare); //change this such that when they start clicking and hold they can mousever, whenthey stop holding then stop mouseover
+        box.style.backgroundColor = 'white';
         board.appendChild(box);
     }
-}
+
+}   
 
 function sizeIndicator(userInput){
-    createGrid(userInput);
+    if (userInput >= 2 && userInput <= 100){
+        createGrid(userInput);
+        
+    }
+    else{
+        console.log("Size too large");
+    }
+  
+
+
+   
 }
 
 sizeIndicator(16); //by having this as a constant it is the default box before getting resized
+
+function colorSquare(){
+
+    this.style.backgroundColor = color;
+
+    // buttonReset.addEventListener('click', () =>{
+    //     this.style.backgroundColor = "white";
+    // }) //this one actually resets
+
+    // buttonClick();
+}
+
+function buttonClick(){
+
+    buttonBlack.addEventListener('click', ()=>{
+        color = "black";
+    })
+
+    buttonErase.addEventListener('click', () => {
+        color = "white";
+    })
+
+
+    // this.style.backgroundColor = "green"; //figured out how to make sure it reamins green after clciking option
+    // buttonInvert;
+    // buttonOpacity;
+    // buttonRandom;
+    
+}
+
+buttonClick();
+
+
 
