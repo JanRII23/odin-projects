b54fc87e438f294f43efb6b4e2a345b94097dc9e
Jan <jrejasii@gmail.com>
2022-05-22 20:45:38 -0500

    still having problems with the toggle of activating when hovering and then clicking

--- odin_etchSketch/java.js
+++ odin_etchSketch/java.js
@@ -1,25 +1,35 @@
-const board = document.querySelector(".flex-container");
+const board_div = document.querySelector(".flex-container");
 
 function makeGrid(){
-    for (let i = 0; i < 16; i++){
-      const row = board.appendChild(document.createElement('div'));
+    for (let i = 0; i < 20; i++){
+      let row = board_div.appendChild(document.createElement('div'));
 
-        for (let j = 0; j < 16; j++){
-            const square = document.createElement('div');
-            square.className = 'box';
-            row.appendChild(square);
+        for (let j = 0; j < 20; j++){
+            let col = document.createElement('div');
+            col.className = 'box';
+            row.appendChild(col);
         }
      
     }
+    const boxes_div = document.querySelectorAll(".box");
+
 }
 
+function changeColor(){
+    boxes_div.classList.toggle('active')
+
+}
+
+boxes_div.addEventListener('click', () =>{
+    changeColor();
+})
 
 
 function main(){
-    board.addEventListener('click', () => {
-        makeGrid();
-    });
-  
+    makeGrid();
+   
 }
 
 main();
+
+
