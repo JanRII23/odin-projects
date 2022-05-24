6fa44c3bad14e01d24b3a5a4f3ad6a3f77d032a1
Jan <jrejasii@gmail.com>
2022-05-24 14:01:57 -0500

    semi-completed basic implementation of etch-a-sketch with takeaways in the java.js at the end. The only thing left to complete is adding some more design via css and javascript

--- odin_etchSketch/java.js
+++ odin_etchSketch/java.js
@@ -1,14 +1,12 @@
-let color = "black"; //Im dumb by making this a global variable it can be changed WTF   
+let color = "black"; 
+let click = false;
 const buttonBlack = document.querySelector('.black');
 const buttonInvert = document.querySelector('.invert');
 const buttonErase = document.querySelector('.erase');
-const buttonOpacity = document.querySelector('.opacity'); //needs to be a slider
 let buttonRandom = document.querySelector('.random');
 const buttonReset = document.querySelector('.reset');
+const buttonFullRandom = document.querySelector('.fullRandom');
 
-
-
-//reveiw the whole script!!
 function createGrid(size){
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
@@ -21,7 +19,7 @@ function createGrid(size){
     for (let i = 0; i < val; i++){
         let box = document.createElement('div');
         box.classList.add('box');
-        box.addEventListener('mouseover', colorSquare); //change this such that when they start clicking and hold they can mousever, whenthey stop holding then stop mouseover
+        box.addEventListener('mouseover', colorSquare); 
         box.style.backgroundColor = 'white';
         board.appendChild(box);
     }
@@ -42,17 +40,23 @@ function sizeIndicator(userInput){
    
 }
 
-sizeIndicator(16); //by having this as a constant it is the default box before getting resized
-
 function colorSquare(){
 
-    this.style.backgroundColor = color;
+    if (click == true){
 
-    // buttonReset.addEventListener('click', () =>{
-    //     this.style.backgroundColor = "white";
-    // }) //this one actually resets
+    if (color === "fullRandom"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } 
+    else{
+        this.style.backgroundColor = color;
+    }
 
-    // buttonClick();
+    }
+
+    buttonReset.addEventListener('click', () =>{
+        this.style.backgroundColor = "white";
+    }) 
+
 }
 
 function buttonClick(){
@@ -65,15 +69,60 @@ function buttonClick(){
         color = "white";
     })
 
+    buttonRandom.addEventListener('click', () =>{
+        color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    })
+
+    buttonInvert.addEventListener('click', () => {
+        invertBoard();
+    })
+
+    
+    buttonFullRandom.addEventListener('click', () => {
+        color = "fullRandom";
+    })
 
     // this.style.backgroundColor = "green"; //figured out how to make sure it reamins green after clciking option
-    // buttonInvert;
-    // buttonOpacity;
-    // buttonRandom;
+    // buttonOpacity; //needs to be a slider
+    
     
 }
 
-buttonClick();
+function invertBoard(){
+    let board = document.querySelector(".board");
+    const elements = board.querySelectorAll('.box');
+    elements.forEach(x => x.classList.toggle('active'));
+    
+}
 
+function startDraw(){
+    click = true;
+}
+
+function main(){
+    sizeIndicator(16); //by having this as a constant it is the default box before getting resized
+    buttonClick();
+
+    document.querySelector("body").addEventListener('click', ()=>{
+        click = !click;
+    })
+}
+
+main();
+
+
+
+// main takeaways
+//  1. Do not forget about the usefulness of global variables. Using let allows you to mutate variables (reassign them)
+//  2. Practice doing the function main() to keep code organized and cacheing the DOM
+//  3. the invertBoard() function is an important example of selecting javascripted DIVS
+//  4. Future change drawing such that when they start clicking and hold they can mousever, whenthey stop holding then stop mouseover to stop drawing
+            // I didn't think you could even do that in the main function doing click = !click CRAZY
+// 5. reset.css should be a staple whenever creating website in that it standardizes to all web browsers
+//    6. whenever you click the features it should turn green and whenever clicking again it turns off
+//   7. practice the commas and separating into new lines
+
+
+//Improvements later add colors in each button, add error when setting such a big size, mode to see if currently drawing, adding a opacity slider, instead of drawing in squares i could change the shape to circle etc.
 
 
