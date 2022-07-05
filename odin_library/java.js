90741d886a53479754cce4ec8f4cf461ff98c8aa
Jan <jrejasii@gmail.com>
2022-07-05 18:40:51 -0500

    solved allowing some overflow of a div using flex-wrap

--- odin_library/java.js
+++ odin_library/java.js
@@ -3,6 +3,8 @@
 // each book gets added as a card or in a table
 // 2 buttons on each card read/not read and remove
 
+//make notes about the library project simulator
+
 const button_div = document.querySelector(".addButton");
 const popUp_div = document.querySelector(".popUp");
 
